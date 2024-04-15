ffb3ca2482614ef3111d9197f17b82bfa73d3347
alkan arslan <arslanalkan356@gmail.com>
2024-04-15 19:23:43 +0300

    Add project files.

---
+++ AcunMedyaFestavaLive/Controllers/DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaFestavaLive.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+
+    }
+}
